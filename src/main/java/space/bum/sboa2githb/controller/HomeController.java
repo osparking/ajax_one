d2f66acb9b510bfd5afdd4f36c05ca376c6e7cab
osparking <jbpark03@gmail.com>
2024-05-16 13:44:36 +0900

    동기적 POST 요청 처리 - 간소화

--- src/main/java/space/bum/sboa2githb/controller/HomeController.java
+++ src/main/java/space/bum/sboa2githb/controller/HomeController.java
@@ -3,7 +3,6 @@ package space.bum.sboa2githb.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -15,8 +14,7 @@ public class HomeController {
 
   @PostMapping("/greet_customer")
   @ResponseBody
-  public String showHome(@RequestParam String lname,
-      @RequestParam String fname) throws InterruptedException {
-    return "<h3>Hello '" + lname + ", " + fname + "'</h3>";
+  public String greets() throws InterruptedException {
+    return "<h3>Hello 동기적 AJAX 요청</h3>";
   }
 }
