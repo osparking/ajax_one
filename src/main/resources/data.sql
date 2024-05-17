1b45845e57ccd59c9e84e72f0f6571a58ce040e1
osparking <jbpark03@gmail.com>
2024-05-17 20:25:14 +0900

    customer 테이블 스키마 및 3 레코드 삽입

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO "customer"("CustomerID", "companyname", "contactname", "address", "city", "postalcode", "country") 
+VALUES('ahnjb', '회사명1', '접촉명1', '주소1', '도시1', '우편번호1', '국가1');
+INSERT INTO "customer"("CustomerID", "companyname", "contactname", "address", "city", "postalcode", "country") 
+VALUES('smhd', '회사명2', '접촉명2', '주소2', '도시2', '우편번호2', '국가2');
+INSERT INTO "customer"("CustomerID", "companyname", "contactname", "address", "city", "postalcode", "country") 
+VALUES('withpwr', '회사명3', '접촉명3', '주소3', '도시3', '우편번호3', '국가3');
