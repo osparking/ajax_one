b3f4b7b3a8460d0f17599753369e49bfe60003fb
osparking <jbpark03@gmail.com>
2024-05-18 15:09:37 +0900

    customer 도메인/모델 클래스

---
+++ src/main/java/space/bum/sboa2githb/domain/Customer.java
@@ -0,0 +1,26 @@
+package space.bum.sboa2githb.domain;
+
+import org.springframework.data.annotation.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class Customer {
+  @Id
+  private int seqno;
+  private String CustomerID;
+  private String companyname;
+  private String contactname;
+  private String address;
+  private String city;
+  private String postalcode;
+  private String country;
+}
